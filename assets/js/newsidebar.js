e3979198aaf62de31c37c287a2df7391a522f567
Paulo Henrique Azevedo Rattes <128646286+PauloHenriDev@users.noreply.github.com>
2023-11-05 23:33:42 -0300

    Add files via upload

--- assets/js/newsidebar.js
+++ assets/js/newsidebar.js
@@ -1,6 +1,8 @@
 const toggleBtn = document.querySelector('.toggle');
 const sidebar = document.querySelector('.sidebar');
+const conteudo = document.querySelector('.conteudo');
 
 toggleBtn.addEventListener('click', function() {
     sidebar.classList.toggle('collapsed');
+    conteudo.classList.toggle('collapsed');
 });
