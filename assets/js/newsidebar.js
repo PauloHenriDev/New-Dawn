58ee3c38fc2bd3edff1d4622a3572b4ac68ce4d8
Paulo Henrique Azevedo Rattes <128646286+PauloHenriDev@users.noreply.github.com>
2023-11-06 00:42:22 -0300

    Add files via upload

--- assets/js/newsidebar.js
+++ assets/js/newsidebar.js
@@ -1,8 +1,40 @@
-const toggleBtn = document.querySelector('.toggle');
+const body = document.querySelector('body');
+const nav = document.querySelector('nav');
+const toggle = document.querySelector(".toggle");
+const searchBtn = document.querySelector(".search-box");
+const modeSwitch = document.querySelector(".bottom-toggle-switch");
+const modeText = document.querySelector(".mode-text");
+const moon = document.querySelector("bx bx-moon icon moon");
+const sun = document.querySelector("bx bx-moon icon sun");
 const sidebar = document.querySelector('.sidebar');
 const conteudo = document.querySelector('.conteudo');
 
-toggleBtn.addEventListener('click', function() {
+toggle.addEventListener('click', function() {
     sidebar.classList.toggle('collapsed');
     conteudo.classList.toggle('collapsed');
 });
+
+
+toggle.addEventListener("click" , () =>{
+    nav.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click" , () =>{
+    nav.classList.remove("close");
+})
+
+function sun_moon() {
+
+    body.classList.toggle('dark')
+
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+        moon.computedStyleMap.display = "none"
+        sun.computedStyleMap.display = "inline-block"
+    }else{
+        modeText.innerText = "Dark mode";
+        sun.computedStyleMap.display = "none"
+        moon.computedStyleMap.display = "inline-block"
+    }
+
+}
